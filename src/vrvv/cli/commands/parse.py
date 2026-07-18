73e09616bfe8cd8e22e44e09b5df95c9c3e0d548
andrewwillowen <60939910+andrewwillowen@users.noreply.github.com>
2026-07-18 13:45:17 -0500

    Initialize CLI setup

--- src/vrvv/cli/commands/parse.py
+++ src/vrvv/cli/commands/parse.py
@@ -1,3 +1,16 @@
 """
 Parse computational output files for data necessary for vrvv.
 """
+
+import typer
+
+app = typer.Typer(
+    help="Parse files for necessary data.",
+    no_args_is_help=True,
+)
+
+
+@app.command("cfour")
+def cfour() -> None:
+    """Use the CFOUR parsing plugin."""
+    typer.echo("This command will invoke the CFOUR parsing plugin.")
--- src/vrvv/logging.py
+++ src/vrvv/logging.py
@@ -1,3 +1,10 @@
 """
 Methods for configuring logging (using loguru).
 """
+
+from loguru import logger
+
+
+def configure_logging(level):
+    """Configure default logging for vrvv package"""
+    pass
